65f83ce5fd601f59833e9494e50c815ab7ba9760
Jimmi Bram <jimmi@bramlabs.io>
2026-04-01 15:17:50 +0200

    Auto-start GhostType at login and launch after install

--- Casks/ghosttype.rb
+++ Casks/ghosttype.rb
@@ -9,6 +9,19 @@ cask "ghosttype" do
 
   app "GhostType.app"
 
+  postflight do
+    system_command "/usr/bin/osascript", args: [
+      "-e", 'tell application "System Events" to make login item at end with properties {path:"/Applications/GhostType.app", hidden:false}'
+    ]
+    system_command "/usr/bin/open", args: ["/Applications/GhostType.app"]
+  end
+
+  uninstall_postflight do
+    system_command "/usr/bin/osascript", args: [
+      "-e", 'tell application "System Events" to delete login item "GhostType"'
+    ], must_succeed: false
+  end
+
   zap trash: [
     "~/Library/Preferences/com.ghosttype.app.plist",
   ]
